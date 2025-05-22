88109e53b981c29c866dda289943ee4f9e9608eb
Jemadiar1 <jedmadiar@hotmail.com>
2025-05-22 13:36:31 -0500

    rutas

--- db.sql
+++ db.sql
@@ -14,3 +14,15 @@ CREATE TABLE productos (
     stock INT,
     precio DECIMAL(10, 2)
 );
+
+CREATE TABLE ventas (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_cliente INT NOT NULL,
+  id_producto INT NOT NULL,
+  cantidad INT NOT NULL,
+  precio_unitario DECIMAL(10,2) NOT NULL,
+  total DECIMAL(10,2),
+  fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (id_cliente) REFERENCES clientes(id),
+  FOREIGN KEY (id_producto) REFERENCES productos(id)
+);
